56939ce701523745ffd97dca6e133bcd0c366d10
Pradeep_Reddy_Kypa <deepu462@gmail.com>
2023-03-23 22:28:49 +0530

    UI for Xylophone

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+// import 'package:audioplayers/audioplayers.dart';
 
 void main() => runApp(XylophoneApp());
 
@@ -8,7 +9,67 @@ class XylophoneApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Center(
+                child: Container(
+                  height: 150.0,
+                  width: double.infinity,
+                  color: Colors.green,
+                  child: TextButton(
+                    onPressed: () {},
+                    child: Text('1'),
+                  ),
+                ),
+              ),
+              Center(
+                child: Container(
+                  height: 150.0,
+                  width: double.infinity,
+                  color: Colors.red,
+                  child: TextButton(
+                    onPressed: () {},
+                    child: Text('2'),
+                  ),
+                ),
+              ),
+              Center(
+                child: Container(
+                  height: 150.0,
+                  width: double.infinity,
+                  color: Colors.blue,
+                  child: TextButton(
+                    onPressed: () {},
+                    child: Text('3'),
+                  ),
+                ),
+              ),
+              Center(
+                child: Container(
+                  height: 150.0,
+                  width: double.infinity,
+                  color: Colors.limeAccent,
+                  child: TextButton(
+                    onPressed: () {},
+                    child: Text('4'),
+                  ),
+                ),
+              ),
+              Center(
+                child: Container(
+                  height: 150.0,
+                  width: double.infinity,
+                  color: Colors.orange,
+                  child: TextButton(
+                    onPressed: () {},
+                    child: Text('5'),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
